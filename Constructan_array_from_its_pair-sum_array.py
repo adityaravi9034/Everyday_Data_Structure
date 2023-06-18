fb8bb4729faa0eaf608701a0c02ef2373d3c2d36
Aditya Ravi <103438753+adityaravi9034@users.noreply.github.com>
2023-06-18 21:25:18 +0200

    Constructan_array_from_its_pair-sum_array.py

--- Constructan_array_from_its_pair-sum_array.py
+++ Constructan_array_from_its_pair-sum_array.py
@@ -1,7 +1,7 @@
 """Given a pair-sum array and size of the original array (n), construct the original array.
 A pair-sum array for an array is the array that contains sum of all pairs in ordered form. For example pair-sum array for arr[] = {6, 8, 3, 4} is {14, 9, 10, 11, 12, 7}.
 In general, pair-sum array for arr[0..n-1] is {arr[0]+arr[1], arr[0]+arr[2], ……., arr[1]+arr[2], arr[1]+arr[3], ……., arr[2]+arr[3], arr[2]+arr[4], …., arr[n-2]+arr[n-1}.
-“Given a pair-sum array, construct the original array.”
+“Given a pair-sum array, construct the original array.  ”
 
 """
 
