38c11d6de56c195ada4d159f57a20f4da12cab2c
Aditya Ravi <103438753+adityaravi9034@users.noreply.github.com>
2023-06-19 22:02:22 +0200

    Inversion_count_in_Array_using_Merge_Sort.py

---
+++ Inversion count in Array using Merge Sort.py
@@ -0,0 +1,32 @@
+"""Inversion Count for an array indicates – how far (or close) the array is from being sorted. If the array is already sorted, then the inversion count is 0, but if the array is sorted in reverse order, the inversion count is the maximum. 
+
+Given an array arr[]. The task is to find the inversion count of arr[]. Where two elements arr[i] and arr[j] form an inversion if a[i] > a[j] and i < j.
+
+Examples: 
+
+Input: arr[] = {8, 4, 2, 1}
+Output: 6
+Explanation: Given array has six inversions: (8, 4), (4, 2), (8, 2), (8, 1), (4, 1), (2, 1).
+
+
+Input: arr[] = {1, 20, 6, 4, 5}
+Output: 5
+Explanation: Given array has five inversions: (20, 6), (20, 4), (20, 5), (6, 4), (6, 5). """
+
+ 
+def getInvCount(arr, n):
+ 
+    inv_count = 0
+    for i in range(n):
+        for j in range(i + 1, n):
+            if (arr[i] > arr[j]):
+                inv_count += 1
+ 
+    return inv_count
+ 
+ 
+# Driver Code
+arr = [1, 20, 6, 4, 5]
+n = len(arr)
+print("Number of inversions are",
+      getInvCount(arr, n))
